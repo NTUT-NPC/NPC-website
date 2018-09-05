e7c0b73928ee05c77aa9fbe1ec8c007f3757e130
DarkFanXing <36408071+darkfanxing@users.noreply.github.com>
2018-09-05 19:03:44 +0800

    v4.0
    v4.0

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 var windowsize = $(window).width();
 
+
 // the windowsize judge
 function CheckSize(windowsize) {
 	if (windowsize < 768) {
 		$('#navbarItem ul').removeClass('LineBorder');
+		$('.nav-link').addClass('hamburger');
+		$(".RWDTitle").removeClass("displayNone");
+		$(".parallax-window-50vh div").addClass('displayNone');
 	} else {
 		$('#navbarItem ul').addClass('LineBorder');
+		$('.nav-link').removeClass('hamburger');
+		$(".RWDTitle").addClass("displayNone");
+		$(".parallax-window-50vh div").removeClass("displayNone");
 	}
 
 	if (windowsize < 820) {
@@ -34,7 +41,42 @@ $(window).resize(function () {
 	CheckSize(windowsize);
 });
 
+// the facebook and CTF
+$(document).ready(function() {
+	$('button').click(function() {
+		var href;
+		if($(this).val() == "Facebook") {
+			href = "https://www.facebook.com/NPC.OwO/";
+		} else {
+			href = "https://www.ntut.club/challenges"
+		}
+		window.open(href);
+		return false;
+	})
+})
+
+
 //the slid click
-$('.contant').click(function () {
-	$('html,body').animate({ scrollTop: $('#contant').offset().top }, 1200);
-});
+// $('.contact').click(function () {
+// 	$(document).bind('mousewheel', function() { return false; });
+// 	$('html,body').animate({ scrollTop: $('#contact').offset().top }, 600);	
+	
+// 	function scroll(judge) {
+// 		$(window).scroll(function() {
+// 			var scroll = $(this).scrollTop();
+// 			var contantH = $("html").height()
+// 			var viewscroll = $(this).height();
+// 			if (judge < scroll) {
+// 				$(document).unbind('mousewheel');
+// 			}
+// 			console.log(judge + "mmm");
+// 			console.log(scroll + "ddd");
+// 			console.log(contantH);
+			
+// 		})
+// 	}
+
+// 	var judge = $("#contact").offset().top;
+// 	scroll(judge);
+	
+// });
