ce55d835c248dd1ce5255556d2e882f5cdf8f72e
DarkFanXing <36408071+darkfanxing@users.noreply.github.com>
2018-09-07 22:34:42 +0800

    v5.0
    v5.0

--- js/AboutUs.js
+++ js/AboutUs.js
@@ -1,1 +1,40 @@
-var test = $('#ClubNumber').html();
+var w = $(window).width();
+
+CheckSize(w);
+
+$(window).resize(function() {
+    var w = $(window).width();
+    CheckSize(w);
+});
+
+function CheckSize(w) {
+    if (w > 800) {
+        $(".img").css({
+        	"width": "130px",
+        	"margin": "0 30px",
+        	"display": "inlin"
+        });
+    } else if (w <= 800 && w > 400) {
+    	$(".img").css({
+        	"width": "150px",
+        	"margin": "0 20px"
+        });
+    } else {
+    	$(".img").css({
+        	"width": "150px",
+        	"margin": "0 10px"
+        });
+    }
+
+    if (w < 500) {
+    	$(".AboutContent").removeClass("pl-5");
+    	$(".AboutContent").removeClass("pr-5");
+    	$(".AboutContent").addClass("pl-3");
+    	$(".AboutContent").addClass("pr-3");
+    } else {
+    	$(".AboutContent").addClass("pl-5");
+    	$(".AboutContent").addClass("pr-5");
+    	$(".AboutContent").removeClass("pl-3");
+    	$(".AboutContent").removeClass("pr-3");
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
-var windowsize = $(window).width();
+var w = $(window).width();
 
 
-// the windowsize judge
-function CheckSize(windowsize) {
-	if (windowsize < 768) {
+
+
+// the w judge
+function CheckSize(w) {
+	if (w < 486) {
+		$('.row p').addClass("displayNone");
+	} else {
+		$('.row p').removeClass('displayNone');
+	}
+
+	if (w < 758) {
 		$('#navbarItem ul').removeClass('LineBorder');
 		$('.nav-link').addClass('hamburger');
 		$(".RWDTitle").removeClass("displayNone");
 		$(".parallax-window-50vh div").addClass('displayNone');
+		
 	} else {
 		$('#navbarItem ul').addClass('LineBorder');
 		$('.nav-link').removeClass('hamburger');
@@ -15,11 +24,12 @@ function CheckSize(windowsize) {
 		$(".parallax-window-50vh div").removeClass("displayNone");
 	}
 
-	if (windowsize < 820) {
+	if (w < 820) {
 		$('.displayText').addClass("displayNone");
 		$('#MainText').removeClass('w-50 mx-auto').addClass('w-100 text-center');
 		$('.content2').removeClass("displayNone");
 		$('.content1').addClass("displayNone");
+		
 	} else {
 		$('.displayText').removeClass("displayNone");
 		$('#MainText').removeClass('w-100 text-center').addClass('w-50 mx-auto');
@@ -27,27 +37,64 @@ function CheckSize(windowsize) {
 		$(".content1").removeClass("displayNone");
 	}
 
-	if (windowsize < 486) {
-		$('.row p').addClass("displayNone");
+	if (w > 700 && w < 900) {
+		$(".parallax-window").css("min-height", "600px");
+	} else if (w > 550 && w <= 700) {
+		$(".parallax-window").css("min-height", "475px");
+	} else if (w <= 550) {
+		$(".parallax-window").attr("data-image-src", "./img/logo.png");
 	} else {
-		$('.row p').removeClass('displayNone');
+		$(".parallax-window").css("min-height", "100vh");
+		$(".parallax-window").attr("data-image-src", "./img/Attach_RWD.jpg");
+	}
+
+	if (w < 1070 && w > 540) {
+		$(".card").css("width", "300px");
+		$("#information").addClass("p-5");
+		$("#information").removeClass("p-0");
+		$("#information .card").addClass("ml-5");
+	} else if (w <= 540 && w > 400 ) {
+		$(".card").css("width", "250px");
+		$("#information").removeClass("p-5");
+		$("#information").removeClass("p-0");
+		$("#information").addClass("pl-4");
+		$("#iformation").addClass("pt-5");
+		$("#information").addClass("pb-5");
+		$("#information .card").removeClass("ml-5");
+		$("#information .card").removeClass("mx-1");
+	} else if(w <= 400) {
+		$(".card").css("width", "220px");
+		$("#information").removeClass("p-5");
+		$("#information").addClass("pl-4");
+		$("#information").addClass("pt-5");
+		$("#information").addClass("pb-5");
+		$("#information .card").removeClass("ml-5");
+		$("#information .card").removeClass("m-5");
+	}
+	if (w > 1440) {
+		$(".card-img-top").attr("height", "250");
+	} else if (w <= 1440 && w > 600) {
+		$(".card-img-top").attr("height", "200");
+	} else if (w <= 600) {
+		$(".card-img-top").attr("height", "140");
 	}
 }
 
-CheckSize(windowsize);
+	
+CheckSize(w);
 
 $(window).resize(function () {
-	var windowsize = $(window).width();
-	CheckSize(windowsize);
+	var w = $(window).width();
+	CheckSize(w);
 });
 
 // the facebook and CTF
 $(document).ready(function() {
-	$('button').click(function() {
+	$('.mybtn').click(function() {
 		var href;
 		if($(this).val() == "Facebook") {
 			href = "https://www.facebook.com/NPC.OwO/";
-		} else {
+		} else if($(this).val() == "CTF") {
 			href = "https://www.ntut.club/challenges"
 		}
 		window.open(href);
@@ -55,28 +102,42 @@ $(document).ready(function() {
 	})
 })
 
+//the img hover
+$(".About").hover(
+   	function() {
+		$(".About").css({
+			"opacity": "0.2",
+			"transition": ".5s"
+		});
+}, 	function() {
+		$(".About").css({
+			"opacity": "1.0",
+			"transition": ".3s"
+		});
+})
 
-//the slid click
-// $('.contact').click(function () {
-// 	$(document).bind('mousewheel', function() { return false; });
-// 	$('html,body').animate({ scrollTop: $('#contact').offset().top }, 600);	
-	
-// 	function scroll(judge) {
-// 		$(window).scroll(function() {
-// 			var scroll = $(this).scrollTop();
-// 			var contantH = $("html").height()
-// 			var viewscroll = $(this).height();
-// 			if (judge < scroll) {
-// 				$(document).unbind('mousewheel');
-// 			}
-// 			console.log(judge + "mmm");
-// 			console.log(scroll + "ddd");
-// 			console.log(contantH);
-			
-// 		})
-// 	}
+$(".Project").hover(
+   	function() {
+		$(".Project").css({
+			"opacity": "0.2",
+			"transition": ".5s"
+		});
+}, 	function() {
+		$(".Project").css({
+			"opacity": "1.0",
+			"transition": ".3s"
+		});
+})
 
-// 	var judge = $("#contact").offset().top;
-// 	scroll(judge);
-	
-// });
+$(".Course").hover(
+   	function() {
+		$(".Course").css({
+			"opacity": "0.2",
+			"transition": ".5s"
+		});
+}, 	function() {
+		$(".Course").css({
+			"opacity": "1.0",
+			"transition": ".3s"
+		});
+})
